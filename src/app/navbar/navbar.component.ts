5b6236def0c866fc199a59fbdffb943a8a37016e
Heidi Engstrom <heidi@embark.com>
2020-07-10 15:41:37 -0400

    move navbar and update

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+  })
+export class NavbarComponent {
+
+}
