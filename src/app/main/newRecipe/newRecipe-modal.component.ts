9296630fe3975ab35c806730b34e3cb17d89730d
Heidi Engstrom <heidi@embark.com>
2020-07-15 12:16:24 -0400

    more updates for build

--- src/app/main/newRecipe/newRecipe-modal.component.ts
+++ src/app/main/newRecipe/newRecipe-modal.component.ts
@@ -3,8 +3,8 @@ import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-new-recipe-modal',
-    templateUrl: './newRecipe.component.html',
-    styleUrls: ['./newRecipe.component.css']
+    templateUrl: './newRecipe-modal.component.html',
+    styleUrls: ['./newRecipe-modal.component.css']
   })
 export class NewRecipeModalComponent implements OnInit {
   constructor(
