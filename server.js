f817b4be9508155d52ac251324834a1f2cdcc5b9
Heidi Engstrom <heidi@embark.com>
2020-07-15 12:09:59 -0400

    work on server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const db = require('./queries')
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -14,5 +14,13 @@ res.sendFile(path.join(__dirname+'/dist/recipe-box-angular/src/index.html'));
 // res.sendFile(path.join(__dirname, '../src' + 'index.html'));
 });
 
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`App running on port ${process.env.PORT || 8080}.`)
+});
