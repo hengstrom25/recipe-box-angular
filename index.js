09ca0e9610bfc1508863a909f37d35aa5a42e760
Heidi Engstrom <heidi@embark.com>
2020-07-15 19:57:26 -0400

    update cors and db

--- index.js
+++ index.js
@@ -3,15 +3,40 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 const port = 3000
+var cors = require('cors');
 
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  })
+  }),
+//   res.header('Access-Control-Allow-Origin', '*')
 )
 
+app.use(cors({origin: 'http://localhost:3000'}));
+
+// Add headers
+// app.use(function (req, res, next) {
+
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+
 app.get('/', (req, res) => {
+    // res.setHeader('Access-Control-Allow-Origin', '*');
     res.json({ info: 'Recipe Box' })
 })
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -18,6 +19,7 @@ import { CategoriesComponent } from './main/categories/categories.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     NgbModule
   ],
