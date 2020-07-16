c46543e2036b4b6e36e03350fd84d7da07b44639
Heidi Engstrom <heidi@embark.com>
2020-07-16 11:12:28 -0400

    clean up index.js for cors work

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(
   bodyParser.urlencoded({
     extended: true,
   }),
-//   res.header('Access-Control-Allow-Origin', '*')
 )
 
 app.use(function(req, res, next) {
@@ -19,26 +18,6 @@ app.use(function(req, res, next) {
 });
 
 // app.use(cors({origin: 'http://localhost:3000'}));
-
-// Add headers
-// app.use(function (req, res, next) {
-
-    // // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // // Request methods you wish to allow
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // // Set to true if you need the website to include cookies in the requests sent
-    // // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // // Pass to next layer of middleware
-    // next();
-// });
 
 app.get('/', (req, res) => {
     // res.setHeader('Access-Control-Allow-Origin', '*');
