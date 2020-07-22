b32e51b857eb86b8563b47e5122d6443f7162757
Heidi Engstrom <heidi@embark.com>
2020-07-22 19:42:02 -0400

    update categories styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { CategoriesComponent } from './main/categories/categories.component';
     HttpClientModule,
     AppRoutingModule,
     NgbModule
+    // FlexLayoutModule
   ],
   providers: [],
   entryComponents: [LoginModalComponent],
