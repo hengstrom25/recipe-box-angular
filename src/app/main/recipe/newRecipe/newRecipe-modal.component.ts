ff5d33837a446f16a3c3abd954451b163ce9f50c
Heidi Engstrom <heidi@embark.com>
2020-07-31 21:44:51 -0400

    save save work for reference

--- src/app/main/recipe/newRecipe/newRecipe-modal.component.ts
+++ src/app/main/recipe/newRecipe/newRecipe-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { of, Observable } from 'rxjs';
-import { catchError, mergeMap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { of, Observable, throwError } from 'rxjs';
+import { catchError, mergeMap, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { NgbModal, NgbActiveModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 import { Recipe } from '../recipe';
@@ -33,15 +33,53 @@ export class NewRecipeModalComponent implements OnInit {
     ngOnInit(): void {
     }
 
-    save(recipe) {
-      recipe = this.model;
+    save(recipe: Recipe): Observable<Recipe> {
+      // const recipe = this.model;
       console.log('recipe', recipe);
-      return this.http.post<any>('http://localhost:3000/recipes', recipe);
+      return this.http.post('http://localhost:3000/recipes', recipe)
       // // Heroku below
       // // return this.http.get('/recipes')
-          // .pipe(mergeMap(res => of({success: true, value: res})),
-          // catchError(err => of({success: false, message: err}))
+          // .pipe(mergeMap(res => {
+          //   return of({ value: res, success: true });
+          // }));
+          // .pipe(catchError(err => console.log('err', err)))
+          // .subscribe()
+          // .pipe(
+          //   map(res => {
+          //     console.log('did something');
+          //     return res;
+          //   }),
+          //   catchError(this.handleError)
+          // );
+          .pipe(map(res => res.json()));
+          // .pipe(
+          //   tap((newRecipe: Recipe) => console.log(`added ${newRecipe}`)),
+          //   catchError(this.handleError)
+          // );
       // );
+    }
+
+    addHero(hero: Hero): Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+    }
+
+    private handleError(error: HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong.
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      // Return an observable with a user-facing error message.
+      return throwError(
+        'Something bad happened; please try again later.');
     }
 
     dismiss(): void { this.modalService.dismissAll(); }
--- src/app/main/recipe/recipe.ts
+++ src/app/main/recipe/recipe.ts
@@ -2,8 +2,8 @@ export class Recipe {
 
     constructor(
       public name: string,
-      public type: string,
-      public link: string,
+      public type?: string,
+      public link?: string,
       public notes?: string,
       public img?: string,
     ) {  }
