53c6d3ac82f5eecca1f42c569d7f7d718da17ce6
Heidi Engstrom <heidi@embark.com>
2021-04-07 17:42:11 -0400

    update

--- queries.js
+++ queries.js
@@ -5,11 +5,6 @@ const pool = new Pool({
   database: 'api',
   password: 'password',
   port: 5432,
-  // user: 'kxmswptopbdhxz',
-  // host: 'ec2-35-153-12-59.compute-1.amazonaws.com',
-  // database: 'd6asbj5kh5obse',
-  // password: '0a197c65223142ca6a223579c069da99426771e34b8d38a611e9e8c4abe9cee2',
-  // port: '5432',
 })
 
 const getUsers = (req, res) => {
